bd8353bd8f09963cc3c60b2ad4b5c441058018ea
mdmsb <msb.arkon@gmail.com>
2022-09-25 23:08:38 +0530

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() => runApp(const MyApp());
 
@@ -26,25 +27,50 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _count = 0;
+  int _count = _getIntFromSharedPref();
 
-  void incrementCounter() {
+  Future<int> _getIntFromSharedPref() async {
+    final prefs = await SharedPreferences.getInstance();
+    final count = prefs.getInt('_count');
+    if (count == null) {
+      return 0;
+    }
+    return count;
+  }
+
+  Future<void> incrementCounter() async {
+    final prefs = await SharedPreferences.getInstance();
     setState(() {
       _count++;
     });
+    await prefs.setInt('_count', _count);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(widget.title)),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            incrementCounter();
-          },
-          style: ,
-          child: Text('$_count'),
+      body: Container(
+        color: Colors.black,
+        child: Center(
+          child: SizedBox(
+            height: 200,
+            width: 200,
+            child: ElevatedButton(
+              onPressed: () {
+                incrementCounter();
+              },
+              style: ElevatedButton.styleFrom(
+                  shape: const CircleBorder(),
+                  padding: const EdgeInsets.all(24),
+                  minimumSize: const Size(200, 40),
+                  backgroundColor: const Color.fromARGB(255, 232, 0, 0)),
+              child: Text(
+                '$_count',
+                style: const TextStyle(fontSize: 50),
+              ),
+            ),
+          ),
         ),
       ),
       drawer: const NavDraw(),
